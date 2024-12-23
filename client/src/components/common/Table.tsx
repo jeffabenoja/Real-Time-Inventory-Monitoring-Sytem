b910749c6855916c8505ed5a2aa4914f906057fb
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2024-12-23 13:26:21 -0600

    Add comments

--- client/src/components/common/Table.tsx
+++ client/src/components/common/Table.tsx
@@ -18,7 +18,7 @@ const Table: React.FC<TableProps> = ({ data, columns }) => {
 
   return (
     <>
-    {/* Search, Buttons, Filter */}
+      {/* Search, Buttons, Filter */}
       <div className='flex items-center justify-between py-2'>
         <div className='flex items-center justify-center'>
           <div className=' relative'>
